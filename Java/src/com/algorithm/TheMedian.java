c924d4059a9fa5c1e93029fbb6b77b3f3dd6131a
truonghoangthuan <tthun32@gmail.com>
2022-01-07 21:31:28 +0700

    TheMedian

---
+++ Java/src/com/algorithm/TheMedian.java
@@ -0,0 +1,30 @@
+package com.algorithm;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class TheMedian {
+    /*
+     * Complete the 'findMedian' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int findMedian(List<Integer> arr) {
+        // Write your code here
+        Collections.sort(arr);
+        int length = arr.size();
+        return arr.get(length/2);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(5);
+        arr.add(7);
+        arr.add(2);
+        arr.add(3);
+        System.out.println(findMedian(arr));
+    }
+}
