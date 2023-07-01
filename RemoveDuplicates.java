b1ad656f9b689f4a143ad5d8f48c4ac3112a773e
truonghoangthuan <tthun32@gmail.com>
2023-07-01 22:45:00 +0700

    Remove Duplicates

---
+++ RemoveDuplicates.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+        System.out.println(removeDuplicates(nums));
+    }
+}
